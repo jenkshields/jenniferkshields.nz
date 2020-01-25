69dbe035b2d9488d478660f0051f599c81587c21
jenniferk <jennifer@jenniferkshields.nz>
2020-01-25 15:47:05 +1300

    add APIs to gatsby-ssr to try debug wrapPageElement display errors on build

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,25 @@
  */
 
 // You can delete this file if you're not using it
+
+const transitionDelay = 500
+
+export const wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>
+}
+
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  if (location.action === "PUSH") {
+    window.setTimeout(() => window.scrollTo(0, 0), transitionDelay)
+  } else {
+    const savedPosition = getSavedScrollPosition(location)
+    window.setTimeout(
+      () => window.scrollTo(...(savedPosition || [0, 0])),
+      transitionDelay
+    )
+  }
+  return false
+}
